7f42a28c386a94d696856f4d9902d56b3dc8bc20
komo3344 <jy940608@naver.com>
2019-11-07 15:11:29 +0900

    페이스북 소셜로그인 로그인만(X)

--- frontend/src/containers/Account/Login/Login.js
+++ frontend/src/containers/Account/Login/Login.js
@@ -1,9 +1,69 @@
+/*global FB*/
 import React, { Component } from 'react'
 import './Login.css'
 import {FacebookLoginButton, GoogleLoginButton} from "react-social-login-buttons"
 
 class Login extends Component {
   state = {
+  }
+
+  componentDidMount() {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId: '2430121853753479', //Change with your Facebook app id
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v3.0'
+      });
+
+      FB.Event.subscribe('auth.statusChange', response => {
+        if (response.authResponse) {
+          this.checkLoginState();
+        } else {
+          console.log('[FacebookLoginButton] User cancelled login or did not fully authorize.');
+        }
+      });
+    };
+  }
+
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  login() {
+    FB.login(this.checkLoginState(), {
+      scope: 'email'
+    });
+  }
+
+  statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      console.log("[FacebookLoginButton] Person is logged into Facebook but not your app");
+    } else {
+      console.log("[FacebookLoginButton] Person is not logged into Facebook");
+    }
+  }
+
+  testAPI() {
+    FB.api('/me', function(response) {
+      console.log('[FacebookLoginButton] Successful login for: ', response);
+    });
   }
 
   handle_change = e => {
@@ -15,7 +75,9 @@ class Login extends Component {
       return newState;
     });
   };
+  
 
+  
   render() {
     return (
       <div className='Login'>
@@ -35,7 +97,7 @@ class Login extends Component {
         </form>
         <button onClick={() => { this.props.display_form('signup') }}> 회원가입 </button><br />
         <div className='SocialLogin'>
-          <FacebookLoginButton onClick={()=> alert("Facebook social login")}/>
+          <FacebookLoginButton onClick={()=> this.login()}/>
           <GoogleLoginButton onClick={()=> alert("Google social login")}/>
         </div>
       </div>
